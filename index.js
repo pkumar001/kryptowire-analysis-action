c8dd5560aea5779bdaeb8d4345a111ad13d7437e
Purn Prem Kumar <pkumar@kryptowire.com>
2020-06-08 10:42:10 -0400

    update path-to-file

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hiiii ${nameToGreet}!`);
+  // `path-to-file` input defined in action metadata file
+  const pathToFile = core.getInput("path-to-file");
+  console.log(`Path: ${pathToFile}!`);
 } catch (error) {
   core.setFailed(error.message);
 }
