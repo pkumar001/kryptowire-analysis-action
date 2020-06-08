1a7902b86cf4303daaeb5f568af533d920f1793a
Purn Prem Kumar <pkumar@kryptowire.com>
2020-06-08 10:36:37 -0400

    remove extra in index

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hiiii ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
