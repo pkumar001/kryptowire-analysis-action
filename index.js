377aa36cd3e21ff115eefbed4657d547b0eda171
Purn Prem Kumar <pkumar@kryptowire.com>
2020-06-08 11:13:18 -0400

    update platform

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const FormData = require("form-data");
+const axios = require("axios").default;
+const fs = require("fs");
 
 try {
   // `path-to-file` input defined in action metadata file
   const pathToFile = core.getInput("path-to-file");
-  console.log(`Path: ${pathToFile}!`);
+  console.log(`Path: ${pathToFile}`);
+
+  // `platform` input defined in action metadata file
+  const platform = core.getInput("platform");
+  console.log(`Platform: ${platform}`);
 } catch (error) {
   core.setFailed(error.message);
 }
