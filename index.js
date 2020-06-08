a98e80e88406c9cb5578fa6bd736cc5435cdf754
Purn Prem Kumar <pkumar@kryptowire.com>
2020-06-08 11:20:40 -0400

    fix index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const FormData = require("form-data");
 const axios = require("axios").default;
 const fs = require("fs");
 
